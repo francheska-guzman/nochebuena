326a89437855ca6da3db67dd08ce3f30b8c2ecca
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-01 16:48:22 -0500

    Updating

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,12 +23,17 @@ export default class HomeScreen extends React.Component {
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
             <Image
-              source={require('../assets/images/robot-dev.png')}
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
               style={styles.welcomeImage}
             />
           </View>
 
           <View style={styles.getStartedContainer}>
+            {this._maybeRenderDevelopmentModeWarning()}
             <Text style={styles.getStartedText}>
               This is the HomeScreen.
             </Text>
@@ -44,6 +49,33 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
 
   _handleGitHubPress = () => {
     WebBrowser.openBrowserAsync(
